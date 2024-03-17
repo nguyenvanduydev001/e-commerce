a059da70ded9c3160ea4b9015160a7a69aeee611
nguyenvanduydev001 <nguyenvanduydev001@gmail.com>
2024-03-17 10:03:48 +0700

    [#3] Hash (Băm) password trong mongodb và handle server error | E-Commerce Project | MERN

--- server/middlewares/errHandler.js
+++ server/middlewares/errHandler.js
@@ -6,9 +6,17 @@ const notFound = (req, res, next) => {
 
 const errHandler = (error, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    let errorMessage = error.message
+    
+    // Kiểm tra xem lỗi có phải là duplicate key error không
+    if (errorMessage.includes('E11000 duplicate key error')) {
+        const email = errorMessage.match(/email: \"([^\"]+)\"/)[1] // Trích xuất email từ thông điệp lỗi
+        errorMessage = `Duplicate key error: Email ${email} already exists.` // Tạo thông điệp lỗi mới
+    }
+
     return res.status(statusCode).json({
         success: false,
-        mes: error?.message
+        mes: errorMessage
     })
 }
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,6 @@ const { notFound, errHandler } = require('../middlewares/errHandler')
 const initRoutes = (app) => {
     app.use('/api/user', userRouter)
 
-
-
     app.use(notFound)
     app.use(errHandler)
 }

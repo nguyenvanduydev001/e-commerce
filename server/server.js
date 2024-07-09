a3af7e99201c6e62a917fc0088b807096de7afcb
nguyenvanduydev001 <nguyenvanduydev001@gmail.com>
2024-07-09 21:48:35 +0700

    commit 1

--- server/server.js
+++ server/server.js
@@ -1,24 +1,25 @@
-const express = require('express')
-require('dotenv').config()
-const dbConnect = require('./config/dbconnect')
-const initRoutes = require('./routes')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
+const express = require("express");
+require("dotenv").config();
+const dbConnect = require("./config/dbconnect");
+const initRoutes = require("./routes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const app = express()
-app.use(cors({
+const app = express();
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
-    methods: ['POST', 'PUT', 'GET', 'DELETE'],
-    credentials: true
-}))
-app.use(cookieParser())
-const port = process.env.PORT || 8888
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-dbConnect()
-initRoutes(app)
-
+    methods: ["POST", "PUT", "GET", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+const port = process.env.PORT || 8888;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+dbConnect();
+initRoutes(app);
 
 app.listen(port, () => {
-    console.log('Server running on the port: ' + port);
-})    
+  console.log("Server running on the port: " + port);
+});
